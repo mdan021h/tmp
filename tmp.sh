ffcfb0862171d9a32c0d772c46a3c80890493d82
mdan021h <d.a.morozoff@gmail.com>
2026-04-05 20:29:56 +0400

    Update tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -1,7 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
+echo "android Tor+obfs4 installer in Termux"
+pkg install tor obfs4proxy -y
+echo "" >> $PREFIX/etc/tor/torrc
+echo "UseBridges 1" >> $PREFIX/etc/tor/torrc
+echo "" >> $PREFIX/etc/tor/torrc
+echo "ClientTransportPlugin obfs4 exec /data/data/com.termux/files/usr/bin/obfs4proxy managed" >> $PREFIX/etc/tor/torrc
+echo "" >> $PREFIX/etc/tor/torrc
 echo >> "Bridge obfs4 185.216.27.184:80 1A9BEBED1D9EC64453DE88499AE>" >> $PREFIX/etc/tor/torrc
 echo >> "Bridge obfs4 185.216.27.184:80 1A9BEBED1D9EC64453DE88499AE>" >> $PREFIX/etc/tor/torrc
 echo >> "Bridge obfs4 57.129.117.251:61919 85F9547D13078145C9D894AF>" >> $PREFIX/etc/tor/torrc
 echo >> "Bridge obfs4 51.38.220.224:30996 22494A012CFA8C88B1D907E2C>" >> $PREFIX/etc/tor/torrc
 echo >> "Bridge obfs4 82.223.216.83:443 3F241ED9439D2E92B48B1730BEE>" >> $PREFIX/etc/tor/torrc
+echo "" >> $PREFIX/etc/tor/torrc
+echo "HTTPTunnelPort 9080" >> $PREFIX/etc/tor/torrc
+echo "" >> $PREFIX/etc/tor/torrc
